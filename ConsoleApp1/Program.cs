4f184668e6ecc28f8db1311a5ece17a76c155eec
moshesc <beepbeep9860@gmail.com>
2019-03-04 19:00:31 +0000

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+   public class Program
     {
+
         static void Main(string[] args)
         {
+            double pay;
+            double afterTax;
+            Staff staff1 = new Staff("Moshe");
+            Console.WriteLine("Insert the hours per month: ");
+            staff1.HoursWorked = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Month: "+Staff.MonthsOfTheYear.February);
+            pay = staff1.CalculatePay();
+            afterTax = staff1.CalculateTax();
+            Console.WriteLine("Net £"+pay);
+            Console.WriteLine("After tax dedction: £ "+afterTax);
         }
     }
 }
---
+++ ConsoleApp1/Staff.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Staff
+    {
+        #region fields
+        private string nameOfStaff;
+        private const double hourlyRate = 30; //Once crated cannot be change
+        private double hWorked;
+        #endregion
+
+        #region Properties
+        public double HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    hWorked = value;
+                }
+                else
+                {
+                    hWorked = 0;
+                }
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void PrintMessage()
+        {
+            Console.WriteLine("Caculate Pay......");
+        }
+
+        public  double CalculatePay()
+        {
+            PrintMessage()
+;
+
+            double staffPay;
+            staffPay = hWorked * hourlyRate;
+
+            if (hWorked > 0 )
+            {
+                return staffPay;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public double CalculateTax()
+        {
+            double afterTax;
+            double staffPay;
+            staffPay = hWorked * hourlyRate;
+            afterTax = staffPay * 0.8;
+            if (hWorked > 0)
+            {
+                return afterTax;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        
+
+        public override string ToString()
+        {
+            return "Name of the staff  = " + nameOfStaff +
+                "Month = " + MonthsOfTheYear.February+
+                "Hourly rate = " + hourlyRate +
+                "Hours worked = " + hWorked;
+        }
+
+        #endregion
+
+        #region Constructor
+        public Staff(string name)
+        {
+            nameOfStaff = name;
+            Console.WriteLine("\n "+nameOfStaff);
+            Console.WriteLine("-------------------");
+        }
+        #endregion
+
+        #region Enum
+        public enum MonthsOfTheYear{
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+
+        }
+        #endregion
+
+    }
+}
